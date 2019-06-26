393bde89c7481d40d7ebca8109a6a170d9defd79
suxuchen <xuchen.su@qq.com>
2019-06-26 09:30:09 +0800

    Add files via upload
    运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
    
    获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
    写入数据 put(key, value) - 如果密钥不存在，则写入其数据值。当缓存容量达到上限时，它应该在写入新数据之前删除最近最少使用的数据值，从而为新的数据值留出空间。
    
    要求在 O(1) 时间复杂度内完成这两种操作。
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/lru-cache

---
+++ lrucache.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <map>
+#include <assert.h>
+#include <stdio.h>
+
+using namespace std;
+
+class node
+{
+public:
+	node(int ikey, int ivalue)
+	{
+		prev = next = NULL;
+		key = ikey;
+		value = ivalue;
+	}
+	~node()
+	{
+	}
+public:
+	int key;
+	int value;
+	node *prev;
+	node *next;
+};
+
+/*双向链表*/
+class doublelist 
+{
+public:
+	doublelist()
+	{
+		head = tail = NULL;
+	}
+	~doublelist()
+	{
+	}
+
+	void move_to_tail(node *pnode)
+	{
+		if(head == pnode) {
+			head = pnode->next;
+		}
+
+		if(tail == pnode)
+			return;
+
+		if(pnode->prev)
+			pnode->prev->next = pnode->next;
+		if(pnode->next)
+			pnode->next->prev = pnode->prev;
+
+		inert_at_tail(pnode);
+	}
+
+	void inert_at_tail(node *pnode)
+	{
+		if(!tail){
+			head = pnode;
+			tail = pnode;
+			pnode->next = NULL;
+			pnode->prev = NULL;
+			return;
+		}
+
+		tail->next = pnode;
+		pnode->prev = tail;
+		pnode->next = NULL;
+		
+		tail = pnode;
+	}	
+
+	int get_head(void)
+	{
+		assert(head != NULL);
+
+		return head->key;
+	}
+
+	void remove_head(void)
+	{
+		if(head != NULL) {
+			head->next->prev = NULL;
+			node *pnode = head;
+			head = head->next;
+			delete pnode;
+		}
+	}
+
+	void dump(void) 
+	{
+		node *pnode = head;
+		while(pnode != NULL) {
+			printf("(%d, %d) - ", pnode->key, pnode->value);
+			pnode = pnode->next;
+		}
+		printf("\n");
+	}
+
+	
+
+private:
+	node *head, *tail;
+};
+
+class lrucache 
+{
+public:
+	lrucache(int size)
+	{
+		maxsize = size;
+	}
+
+	~lrucache()
+	{
+		
+	}
+
+	int get(int key)
+	{
+		map<int, node *>::iterator it;
+		it = lrumap.find(key);
+		/*不存在则返回-1 */
+		if(it == lrumap.end()) {
+			return -1;
+		}
+		/*如果存在则将该节点移动到链表尾，并返回value */
+		node *pnode = it->second;
+		lrulist.move_to_tail(pnode);
+		return pnode->value;
+	}
+
+	void put(int key, int value)
+	{
+		map<int, node*>::iterator it;
+		it = lrumap.find(key);
+		/*存在则更新节点，并将节点移动到链表尾*/
+		if(it != lrumap.end()) {
+			node *pnode = it->second;
+			pnode->value = value;
+			lrulist.move_to_tail(pnode);
+			return;
+		}
+
+		/*如果不存在且没有超限，插入节点并将节点移动到链表尾*/
+		if(lrumap.size() < maxsize) {
+			node *pnode = new node(key, value);
+			lrumap.insert(make_pair(key, pnode));
+			lrulist.inert_at_tail(pnode);
+		} else { /*否则删除链表头元素，插入新节点，并将节点移动到链表尾*/
+			lrumap.erase(lrulist.get_head());
+			lrulist.remove_head();
+			node *pnode = new node(key, value);
+			lrumap.insert(make_pair(key, pnode));
+			lrulist.inert_at_tail(pnode);
+		}
+	}
+
+	void dump(void)
+	{
+		map<int, node*>::iterator it;
+		for(it = lrumap.begin(); it != lrumap.end(); it++) {
+			printf("<%d, %d> - ", it->first, it->second->value);
+		}
+		printf("\n");
+		lrulist.dump();
+	}
+
+private:
+	map<int, node *> lrumap;
+	doublelist lrulist;
+	int maxsize;
+};
+
+int main(void) 
+{
+	lrucache *cache = new lrucache(3);
+	cache->put(1, 1);
+	cache->put(2, 2);
+	cache->put(3, 10);
+	cache->dump();
+	int value = cache->get(1);
+	printf("cache get %d value %d\n", 1, value);
+	value = cache->get(2);
+	printf("cache get %d value %d\n", 2, value);
+	cache->dump();
+	cache->put(4, 4);
+	cache->put(5, 5);
+	cache->put(6, 6);
+	cache->put(7, 7);
+	cache->dump();
+	value = cache->get(3);
+	printf("cache get %d value %d\n", 3, value);
+	value = cache->get(6);
+	printf("cache get %d value %d\n", 6, value);
+	cache->dump();
+	
+	return 0;
+	
+}
